7b8a3cd1de2dbdf57439eb047f66759f2365151e
Joel Bouwkamp <jbouwkamp@stoneplasticsmfg.com>
2013-08-20 19:25:50 -0400

    add some default roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,11 @@
 class Role < ActiveRecord::Base
+  ROLES = {
+    admin: find_or_create_by(description: 'Admin').id,
+    coordinator: find_or_create_by(description: 'Coordinator').id,
+    student: find_or_create_by(description: 'Student').id
+  }
   has_many :roleships
   has_many :users, through: :roleships
 
-  # STUDENT => 'student'
-  # ADMIN = 'admin'
+  
 end
