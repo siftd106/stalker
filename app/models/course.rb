faf7faeae60fcd3ba2a6e58f0e21d19a0007893a
Kurt Hansen <khansen@highfxmedia.com>
2013-09-24 18:49:09 -0400

    Made course description and course start date required fields

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ActiveRecord::Base
+	validates :description, presence: true
+	validates :start_date, presence: true
+
   has_many :enrollments
   has_many :users, through: :enrollments
 
