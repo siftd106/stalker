431278ffe91bf6d5e89b81dd07a89f737f002bd4
Joel <siftd106@yahoo.com>
2014-03-04 19:35:39 -0500

    whoops

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130806232804) do
+ActiveRecord::Schema.define(version: 20130820020354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20130806232804) do
 
   create_table "roles", force: true do |t|
     t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roleships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -66,7 +73,6 @@ ActiveRecord::Schema.define(version: 20130806232804) do
     t.string   "last_name"
     t.string   "phone"
     t.string   "company_name"
-    t.integer  "user_type_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
